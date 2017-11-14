2b63756208d4a1759557c2385e2668fa56c28fb5
dissectorz <toshikverma@yahoo.com>
2017-11-14 13:13:35 +0530

    added

--- src/controller/user.js
+++ src/controller/user.js
@@ -230,8 +230,8 @@ Login.findOne({email:req.body.email},(err,login)=>{
  var transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-      user: 'toshikverma@gmail.com', // Your email id
-      pass: 'anu123anu' // Your password
+      user: 'toshiktest@gmail.com', // Your email id
+      pass: '123123123' // Your password
   }
 });
 var templateString = fs.readFileSync('views/welcome.ejs', 'utf-8');
