dab94adceed13820bac77974f117724aaa705a01
dissectorz <toshikverma@yahoo.com>
2017-11-17 00:51:43 +0530

    put your message here

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
   "port": 8080,
-  "mongoUrl": "mongodb://localhost:27017/rentophila",
+  "mongoUrl": "mongodb://toshik:test@ds111476.mlab.com:11476/rentophila",
   "bodyLimit": "4524kb",
   "myurl":"http://localhost:3005/v1/"
 }
