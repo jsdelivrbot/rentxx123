90ab356544e942d4788b5e61761352012d05b2b8
dissectorz <toshikverma@yahoo.com>
2017-11-17 01:06:37 +0530

    added files

--- src/index.js
+++ src/index.js
@@ -26,7 +26,8 @@ app.get('/',function(req,res){
   
   res.send("home");    
   })
-app.server.listen(config.port);
+app.server.listen(app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 
 console.log(`Started on port ${app.server.address().port}`);
 
