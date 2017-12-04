143a20b3d4cad25533ce37ac395f9ec72c26e840
dissectorz <toshikverma@yahoo.com>
2017-12-04 11:56:40 +0530

    added hearders

--- src/index.js
+++ src/index.js
@@ -22,6 +22,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 app.use('/assets',express.static(__dirname+'/public'));
 app.use('/v1', routes);
+app.use(function (req, res, next) {
+  
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+  
+      // Pass to next layer of middleware
+      next();
+  });
 app.get('/',function(req,res){
   
   res.send("home");    
