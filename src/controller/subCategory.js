68878f2f5e861b3c9320158444bc1659ad0e356c
dissectorz <toshikverma@yahoo.com>
2017-12-29 14:54:58 +0530

    edit subcat

--- src/controller/subCategory.js
+++ src/controller/subCategory.js
@@ -191,7 +191,7 @@ export default({ config, db }) => {
 
     //get subCategory here
     api.post('/get', (req, res) => {
-      Category.find({}, function(err, subcat) {
+      SubCategory.find({}, function(err, subcat) {
         res.json({"subCategories":subcat});
     });
 });
