22c8c6e26b8a014e1ff8e8ec584bb3b06b1c49fa
dissectorz <toshikverma@yahoo.com>
2018-01-03 11:51:43 +0530

    login

--- src/controller/login.js
+++ src/controller/login.js
@@ -37,13 +37,17 @@ User.findOne({email:req.body.email},(err,user)=>{
                             res.status(500).send(err);
                         }
                         user.token=loginDetailsAfterSaving.token; 
-                        let token=jsonwebtoken.sign(user,"example1");   
+                        let token=jsonwebtoken.sign(user,"example1", {
+  expiresIn: 604800 // 1 week
+});   
                         res.status(200).json({token:token});
                     });
 
                 }else{  //user is already logged in
                      user.token=loginDetails.token; 
-                        let token=jsonwebtoken.sign(user,"example1"); 
+                        let token=jsonwebtoken.sign(user,"example1", {
+  expiresIn: 604800 // 1 week
+});
                     res.status(200).json({token:token});
 
                 }
