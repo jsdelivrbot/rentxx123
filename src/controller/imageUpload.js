99d4522b206d8159a7bf7dc8e2e6e1a8fc905d0b
dissectorz <toshikverma@yahoo.com>
2018-01-09 21:47:08 +0530

    multer

--- src/controller/imageUpload.js
+++ src/controller/imageUpload.js
@@ -4,23 +4,60 @@ export default({ config, db }) => {
   let api = Router();
 
   
-api.post('/', multer({ dest: 'uploads'}).single('upl'), function(req,res){
-	console.log(req.body); //form fields
-	/* example output:
-	{ title: 'abc' }
-	 */
-	console.log(req.file); //form files
-	/* example output:
-            { fieldname: 'upl',
-              originalname: 'grumpy.png',
-              encoding: '7bit',
-              mimetype: 'image/png',
-              destination: './uploads/',
-              filename: '436ec561793aa4dc475a88e84776b1b9',
-              path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
-              size: 277056 }
-	 */
-	res.status(204).end();
-});
+const upload = multer({
+    dest:'images/', 
+    limits: {fileSize: 10000000, files: 1},
+    fileFilter:  (req, file, callback) => {
+    
+        if (!file.originalname.match(/\.(jpg|jpeg)$/)) {
+
+            return callback(new Error('Only Images are allowed !'), false)
+        }
+
+        callback(null, true);
+    }
+}).single('image')
+
+api.post('/', (req, res) => {
+
+    upload(req, res, function (err) {
+
+        if (err) {
+
+            res.status(400).json({message: err.message})
+
+        } else {
+
+            let path = `/images/${req.file.filename}`
+            res.status(200).json({message: 'Image Uploaded Successfully !', path: path})
+        }
+    })
+})
+
+api.get('/images/:imagename', (req, res) => {
+
+    let imagename = req.params.imagename
+    let imagepath = __dirname + "/images/" + imagename
+    let image = fs.readFileSync(imagepath)
+    let mime = fileType(image).mime
+
+	res.writeHead(200, {'Content-Type': mime })
+	res.end(image, 'binary')
+})
+
+
+
+
+api.use((err, req, res, next) => {
+
+    if (err.code == 'ENOENT') {
+        
+        res.status(404).json({message: 'Image Not Found !'})
+
+    } else {
+
+        res.status(500).json({message:err.message}) 
+    } 
+})
   return api;
 }
