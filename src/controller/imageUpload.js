d23b600fd5dc521dee8b0cc3cd4d46a8e6691706
dissectorz <toshikverma@yahoo.com>
2018-01-10 12:47:14 +0530

    aws-sdk

--- src/controller/imageUpload.js
+++ src/controller/imageUpload.js
@@ -1,62 +1,36 @@
 import { Router } from 'express';
 import multer from 'multer';
 import fs from 'fs';
+import aws from 'aws-sdk';
+import multerS3 from 'multer-s3';
 export default({ config, db }) => {
   let api = Router();
 
- const upload = multer({
-    dest:'uploads/', 
-    limits: {fileSize: 10000000, files: 1},
-    fileFilter:  (req, file, callback) => {
+aws.config.update({
+    secretAccessKey: 'vaLW99gpg8sUkmGF2KT4aP1S8tMzRLBAqv0S3pf5',
+    accessKeyId: 'AKIAIWCDVYX3H4ND4Q5A'
     
-        if (!file.originalname.match(/\.(jpg|jpeg)$/)) {
+});
 
-            return callback(new Error('Only Images are allowed !'), false)
-        }
 
-        callback(null, true);
-    }
-}).single('profileImage')
+  var  s3 = new aws.S3();
 
-api.post('/', (req, res) => {
 
-    upload(req, res, function (err) {
-
-        if (err) {
-
-            res.status(400).json({message: err.message})
-
-        } else {
-
-            let path = `/images/${req.file.filename}`
-            res.status(200).json({message: 'Image Uploaded Successfully !', path: path})
+var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'rentophilaimages',
+        key: function (req, file, cb) {
+            console.log(file);
+            cb(null, file.originalname); //use Date.now() for unique file keys
         }
     })
-})
+});
 
-api.get('/images/:imagename', (req, res) => {
 
-    let imagename = req.params.imagename
-    let imagepath = __dirname + "/uploads/" + imagename
-    let image = fs.readFileSync(imagepath)
-    let mime = fileType(image).mime
-
-	res.writeHead(200, {'Content-Type': mime })
-	res.end(image, 'binary')
-})
-
-api.use((err, req, res, next) => {
-
-    if (err.code == 'ENOENT') {
-        
-        res.status(404).json({message: 'Image Not Found !'})
-
-    } else {
-
-        res.status(500).json({message:err.message}) 
-    } 
-})
-
+api.post('/', upload.array('profileImage',1), function (req, res, next) {
+    res.send("Uploaded!");
+});
 
   return api;
 }
