b2a26e5cb43628f9e0490ffce10163b36eceabda
dissectorz <toshikverma@yahoo.com>
2018-04-22 15:20:02 +0530

    add

--- src/controller/spam.js
+++ src/controller/spam.js
@@ -2,10 +2,18 @@ import mongoose from 'mongoose';
 import { Router } from 'express';
 import Spam from '../model/spam';
 import bodyParser from 'body-parser';
+import Pusher from 'pusher';
 export default({ config, db }) => {
   let api = Router();
 //adding a city
 //v1/city/add
+let pusher = new Pusher({
+  appId: '513442',
+  key: 'dd20d8a12a2c4b1c7960',
+  secret: 'fde09d5acecffcb9f07d',
+  cluster: 'ap2',
+  encrypted: true
+});
   api.post('/add', (req, res) => {
     //check password or match password
        //token matching and only admin can add
@@ -19,7 +27,13 @@ export default({ config, db }) => {
                spam.save((err,spam_got)=>{
 
                 if(!err){
-
+pusher.trigger('os-poll', 'os-vote', {
+                     gender:req.body.gender,
+                     partId:req.body.partId,
+                     age:req.body.age,
+                     isVoted:req.body.isVoted,
+                     document:req.body.document
+});
                     res.status(200).send(spam_got);
                 }else{
                     res.status(500).send(err);
--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ app.use(cors());
 app.use('/assets',express.static(__dirname+'/public'));
 app.use('/v1', routes);
 app.get('/',function(req,res){
-  res.json(__dirname);
-   res.sendFile('/index.html', {root: __dirname }); 
+
+   res.sendFile(path.join(__dirname,'index.html')); 
   })
 app.server.listen(app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
